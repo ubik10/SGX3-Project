99f5727d80d7fc735d2db16c769547e7856cd9ca
Charlie Dey <charlie.tacc.utexas.edu>
2025-06-17 19:02:26 +0000

    added new files

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# More lines of code
+# More lines of code
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return "Hello World!\n"
+
+
+@app.route('/<name>', methods=['GET'])
+def hello_name(name):
+    return f'Hello, {name}!\n'
+
+#the last line of your flask application
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8076)
+
